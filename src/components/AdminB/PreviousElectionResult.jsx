7393ae73dbe443cf9cef834535231f79bfb691f2
Kosala123 <kosalakumara2001@gmail.com>
2025-10-30 23:59:58 +0530

    Updated Previous Election Part

--- src/components/AdminB/PreviousElectionResult.jsx
+++ src/components/AdminB/PreviousElectionResult.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   ArcElement,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+// import { FiArrowLeft, FiChevronDown, FiChevronUp } from "react-icons/fi";
+// import { useNavigate } from "react-router-dom";
+// // Register ChartJS components
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   ArcElement,
+//   Tooltip,
+//   Legend
+// );
 
 const PreviousElectionResult = () => {
-  return (
-    <div>PreviousElectionResult</div>
-  )
-}
+  // const navigate = useNavigate();
 
-export default PreviousElectionResult
+  return <div>PreviousElectionResult</div>;
+};
+
+export default PreviousElectionResult;
